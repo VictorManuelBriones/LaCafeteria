377f7b99e18c033268baff0117862ebca4f07c75
Victor Manuel Briones <victorm140399@gmail.com>
2020-06-03 16:16:19 -0500

    Mensajes asincronos con JS (2) Desarrollo

--- messenger/models.py
+++ messenger/models.py
@@ -11,9 +11,30 @@ class Message(models.Model):
     class Meta:
         ordering = ['created']
 
+class ThreadManager(models.Manager):
+    def find(self, user1, user2):
+        queryset = self.filter(users=user1).filter(users=user2)
+        if len(queryset) > 0:
+            return queryset[0]
+        return None
+
+    def find_or_create(self, user1, user2):
+        thread = self.find(user1, user2)
+        if thread is None:
+            thread = Thread.objects.create()
+            thread.users.add(user1, user2)
+        return thread
+        
+
 class Thread(models.Model):
-    user = models.ManyToManyField(User, related_name='threads')
+    users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = ThreadManager()
+
+    class Meta:
+        ordering = ['-updated']
 
 
 def messages_changed(sender, **kwargs):
@@ -27,10 +48,13 @@ def messages_changed(sender, **kwargs):
         for msg_pk in pk_set:
             msg = Message.objects.get(pk=msg_pk)
             if msg.user not in instance.users.all():
-                print("Ups, ({}) no forma parte del hilo", format(msg.user))
+                print("Ups, ({}) no forma parte del hilo".format(msg.user))
                 false_pk_set.add(msg_pk)
 
-    pk_set.difference_update(false_pk_set)
-    
-m2m_changed.connect(messages_changed, sender=Thread.messages.through)
+        # Forzar la actualización haciendo save
+        instance.save()
 
+    # Borramos de pk_set los mensajes que concuerdan con los de false_pk_set
+    pk_set.difference_update(false_pk_set)
+
+m2m_changed.connect(messages_changed, sender=Thread.messages.through)
